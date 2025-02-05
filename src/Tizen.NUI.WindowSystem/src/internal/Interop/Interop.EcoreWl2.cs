8df62d8bb7eeeaa3bf7e0e2179ab8af3a427da1e
pjh9216 <jh9216.park@samsung.com>
2025-02-05 12:54:07 +0900

    [PoC] Apply tizen-core-wayland
    - Changed code using ecore-wl2
    
    Signed-off-by: pjh9216 <jh9216.park@samsung.com>

--- src/Tizen.NUI.WindowSystem/src/internal/Interop/Interop.EcoreWl2.cs
+++ src/Tizen.NUI.WindowSystem/src/internal/Interop/Interop.EcoreWl2.cs
@@ -8,9 +8,9 @@ namespace Tizen.NUI.WindowSystem
     {
         internal static partial class EcoreWl2
         {
-            const string lib = "libecore_wl2.so.1";
+            const string lib = "libtizen-core-wayland.so.0";
 
-            [global::System.Runtime.InteropServices.DllImport(lib, EntryPoint = "ecore_wl2_window_id_get")]
+            [global::System.Runtime.InteropServices.DllImport(lib, EntryPoint = "tizen_core_wayland_get_window_id")]
             internal static extern int GetWindowId(IntPtr win);
         }
     }
